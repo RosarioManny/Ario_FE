8d602afa6cba2de7e3a8f7f7e480e1e45c8b592d
RosarioManny <rosario.emm47@gmail.com>
2026-04-19 16:17:18 -0400

    Navbar

--- src/components/layout/Navbar/_mobile_navbar.tsx
+++ src/components/layout/Navbar/_mobile_navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation} from "react-router-dom"
 import { _Decorator_Line, _Decorator_PenNib } from "../../../assets/SVGs/Decorator_svgs"
 import { BurgerLine } from "./components/m_nav_burgerLine"
 
@@ -8,10 +8,10 @@ export const MobileNavbar = () => {
   // TODO: Open menu on click, close on clikc outside
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [hasScrolled, setHasScrolled] = useState(false)
-  const [hasLoaded, setHasLoaded] = useState(false)
-
   const [popStage, setPopStage] = useState<'hidden' | 'big' | 'normal'>('hidden')
   const [textVisible, setTextVisible] = useState(false)
+  const location = useLocation()
+  const isHomePage = location.pathname === "/"
 
   useEffect(() => {
     const t1 = setTimeout(() => setPopStage('big'), 100)      // pop out big
@@ -35,13 +35,14 @@ export const MobileNavbar = () => {
 
   // TODO: Add menu links
   const menuLinks = [
+    { name: "Home" , path: ""},
     { name: "contacts" , path: "contacts"},
     { name: "services" , path: "services"},
     { name: "about" , path: "about"},
     { name: "portfolio" , path: "portfolio"},
     { name: "labs" , path: "labs"},
-    { name: "faq" , path: "faq"},
   ]
+
   return (
   <nav className="mobile-navbar">
 
@@ -96,18 +97,34 @@ export const MobileNavbar = () => {
     <div className={`
       fixed top-0 left-0 w-full h-screen 
       bg-void text-white 
-      flex flex-col items-center justify-center space-y-8 
+      flex flex-col items-center justify-center space-y-6
       transition-transform duration-700 ease-in-out z-40
       ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-      <ul className="flex flex-col items-center space-y-6">
+      <img className="size-full object-cover opacity-5 absolute" src="src/assets/images/design/background/scratched_Texture_Transparent.png" alt="" />
+      <ul className="flex flex-col items-center z-40 gap-4 text-mist">
         {menuLinks.map(({name, path}) => (
-          <li key={path} className="comico-font text-2xl uppercase tracking-widest">
+          <li key={path} className="comico-font text-2xl tracking-widest">
             <Link to={`/${path}`} onClick={() => setIsMenuOpen(false)}>
               {name}
             </Link>
           </li>
         ))}
       </ul>
+      { !isHomePage &&
+        <Link to="/">
+          <div className="" >
+              <img 
+              className="size-50 absolute bottom-10 left-8"
+              src="src/assets/images/design/logo/raining_rose_logo_512x512.png" 
+              alt="Logo" 
+            />
+            <h1 className="
+              header-text absolute bottom-26 text-mist text-4xl">
+              <span className=" text-6xl">A</span>rio
+            </h1>
+          </div>
+        </Link>
+      }
     </div>
   </nav>
 )
